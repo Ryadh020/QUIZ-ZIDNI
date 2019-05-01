9cf8f4dea5cf52503706ecded6dcccc6fdb10df9
riyadh <riyadh02220@gmail.com>
2019-05-01 19:06:50 +0200

    reset audios

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const secSuggestion = document.querySelector("#secSuggestion");
 const thrSuggestion = document.querySelector("#thrSuggestion");
 
 // LOAD SOUNDS :
+let first = new Audio();
+first.src = "sounds/first-question.mp3";
+
+let stress = new Audio();
+stress.src = "sounds/stress.mp3";
 
 let tRuE = new Audio();
 tRuE.src = "sounds/true.mp3";
@@ -14,9 +19,9 @@ let fAlSe = new Audio();
 fAlSe.src = "sounds/false.mp3";
 // QUIZ'S : 
 const quiz = [
-        a = "1 + 1",
-        b = " 5 * 2",
-        c = "15 / 3"
+    [a = "1 + 1", number = 1],
+    [b = "5 * 2", number = 1],
+    [c = "15 / 3", number = 3]       
 ]
 
 const suggestions = [
@@ -42,24 +47,26 @@ let randomQ = 0;
 
 // PICK QUESTIONS :
 function pickSuestion(rand) {
-        setTimeout(() => {
-            question.innerHTML = quiz[rand];
+            question.innerHTML = quiz[rand][0];
 
             frsSuggestion.innerHTML = suggestions[rand][0][0];
             secSuggestion.innerHTML = suggestions[rand][1][0];
             thrSuggestion.innerHTML = suggestions[rand][2][0];
-
-        }, 100);
 }
 
 // RUN IT :
 question.addEventListener("click",()=> {
-    pickSuestion(0);
+    first.play();
+    setTimeout(()=> {
+        pickSuestion(0);
+    },10000);
+    
 })
 
 // CHOOSE A SUGGESTION :
 frsSuggestion.addEventListener("click",()=> {
-
+    stress.play();
+    frsSuggestion.style.backgroundImage = 'url(images/stress.png)';
     setTimeout(()=>{
     if(suggestions[randomQ][0][1] === true) {
         frsSuggestion.style.backgroundImage = 'url(images/true.png)';
@@ -68,13 +75,13 @@ frsSuggestion.addEventListener("click",()=> {
         frsSuggestion.style.backgroundImage = 'url(images/fals.png)';
         fAlSe.play();
     }
-    },1000)
+    },11000)
 
     setTimeout(()=>{
         frsSuggestion.style.backgroundImage = 'url(images/suggestion.png)';
         randomQ++;
         pickSuestion(randomQ);
-    },2000)
+    },17000)
     
 
     if(randomQ === quiz.length) {
@@ -84,7 +91,8 @@ frsSuggestion.addEventListener("click",()=> {
 })
 
 secSuggestion.addEventListener("click",()=> {
-
+    stress.play();
+    secSuggestion.style.backgroundImage = 'url(images/stress.png)';
     setTimeout(()=>{
     if(suggestions[randomQ][1][1] === true) {
         secSuggestion.style.backgroundImage = 'url(images/true.png)';
@@ -93,13 +101,13 @@ secSuggestion.addEventListener("click",()=> {
         secSuggestion.style.backgroundImage = 'url(images/fals.png)';
         fAlSe.play();
     }
-    },1000)
+    },11000)
 
     setTimeout(()=>{
         secSuggestion.style.backgroundImage = 'url(images/suggestion.png)';
         randomQ++;
         pickSuestion(randomQ);
-    },2000)
+    },17000)
 
     if(randomQ === quiz.length) {
         pickSuestion(0);
@@ -108,7 +116,8 @@ secSuggestion.addEventListener("click",()=> {
 })
 
 thrSuggestion.addEventListener("click",()=> {
-
+    stress.play();
+    thrSuggestion.style.backgroundImage = 'url(images/stress.png)';
     setTimeout(()=>{
     if(suggestions[randomQ][2][1] === true) {
         thrSuggestion.style.backgroundImage = 'url(images/true.png)';
@@ -117,13 +126,13 @@ thrSuggestion.addEventListener("click",()=> {
         thrSuggestion.style.backgroundImage = 'url(images/fals.png)';
         fAlSe.play();
     }
-    },1000)
+    },11000)
 
     setTimeout(()=>{
         thrSuggestion.style.backgroundImage = 'url(images/suggestion.png)';
         randomQ++;
         pickSuestion(randomQ);
-    },2000)
+    },17000)
 
     if(randomQ === quiz.length) {
         pickSuestion(0);
