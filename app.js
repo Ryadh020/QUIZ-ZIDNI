800b6c9feeaf13f44b9c4691210880a1d2b3d892
riyadh <riyadh02220@gmail.com>
2019-05-02 16:27:56 +0200

    shox right and false ansewrs

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // GET IN THE DOM :
+const questionNumber = document.querySelector("header");
+
 const question = document.querySelector("#question");
 
 const frsSuggestion = document.querySelector("#frsSuggestion");
@@ -19,34 +21,60 @@ let fAlSe = new Audio();
 fAlSe.src = "sounds/false.mp3";
 // QUIZ'S : 
 const quiz = [
-    [a = "1 + 1", number = 1],
-    [b = "5 * 2", number = 1],
-    [c = "15 / 3", number = 3]       
+    [a = "سورة سمعها النجاشي ملك الحبشة فاضت لها دموعه فما هي ؟", number = "السؤال الاول"],
+    [b = "ما هي المادة المسئولة عن تلون جسم الإنسان بالألوان الغامضة ؟", number = "السؤال الثاني"],
+    [c = "من هي الدوله الوحيدة في العالم التي ليس لديها جيش ؟", number = "السؤال الثالث"],
+    [d = "من هو مخترع الطائرة ذات المحرك ؟", number = "السؤال الرابع"],
+    [e = "ماعدد الايام الذي خلقت فيه الارض والسماء ؟", number = "السؤال الخامس"],
+    [f = "ماهو النهر الوحيد في العالم ينبع من الجنوب إلى الشمال ؟", number = "السؤال السابع"],
+    [g = "ماهو علم السيتولوجيا ؟", number = "السؤال الثامن"]       
 ]
 
 const suggestions = [
     [
-        [a = "2", validity = true],
-        [b = "5", validity = false],
-        [c = "15", validity = false]
+        [a = "سورة مريم", validity = true],
+        [b = "سورة الاخلاص", validity = false],
+        [c = "سورة الفاتحة", validity = false]
     ],
     [
-        [a = "14", validity = false],
-        [b = "7", validity = false],
-        [c = "10", validity = true]
+        [a = "قرنية العين", validity = false],
+        [b = "الخلايا العصبية", validity = false],
+        [c = " هي مادة الميلانين", validity = true]
     ],
     [
-        [a = "25", validity = false],
-        [b = "5", validity = true],
-        [c = "0.7", validity = false]
+        [a = "سويسرا", validity = true],
+        [b = "كندا ", validity = false],
+        [c = "السودان ", validity = false]
+    ],
+    [
+        [a = "الاخوين رايت", validity = true],
+        [b = "ابراهام طليطله", validity = false],
+        [c = "جورج برنادشو", validity = false]
+    ],
+    [
+        [a = " سبعة ايام", validity = false],
+        [b = " عشرة ايام", validity = false],
+        [c = " سته ايام", validity = true]
+    ],
+    [
+        [a = "الدانوب", validity = false],
+        [b = "النيل ", validity = true],
+        [c = "الامازون", validity = false]
+    ],
+    [
+        [a = "علم الخلايا", validity = true],
+        [b = "علم الاجناس", validity = false],
+        [c = "علم المكان", validity = false]
     ]
 ]
 
 // VARIABLES :
 let randomQ = 0;
 
-// PICK QUESTIONS :
+// PICK QUESTIONS & QUESTION NUMBER & SUGGESTIONS:
 function pickSuestion(rand) {
+            questionNumber.innerHTML = quiz[rand][1];
+
             question.innerHTML = quiz[rand][0];
 
             frsSuggestion.innerHTML = suggestions[rand][0][0];
@@ -54,14 +82,39 @@ function pickSuestion(rand) {
             thrSuggestion.innerHTML = suggestions[rand][2][0];
 }
 
+
+
+// CHOISE TRUE QUESTION :
+let truth =-1 ;
+
+function chooseTrue(suggestion) {
+    for( let i = 0; i<=suggestions.length; i++) {
+        if(suggestions[randomQ][i][1] === true) {
+            suggestion.style.backgroundImage = 'url(images/true.png)';
+            truth += 1;
+        }
+    }
+}
+
+
+// CHOISE TRUE QUESTION :
+function chooseTrueToo(num,suggestion) {
+    if(suggestions[randomQ][num][1] === true) {
+        suggestion.style.backgroundImage = 'url(images/true.png)';
+    }
+}
+
+
 // RUN IT :
 question.addEventListener("click",()=> {
-    first.play();
+    /*first.play();*/
     setTimeout(()=> {
         pickSuestion(0);
-    },10000);
+        questionNumber.innerHTML = quiz[0][1];
+    },/*10000*/);
     
 })
+
 
 // CHOOSE A SUGGESTION :
 frsSuggestion.addEventListener("click",()=> {
@@ -78,11 +131,20 @@ frsSuggestion.addEventListener("click",()=> {
     },11000)
 
     setTimeout(()=>{
+    if(suggestions[randomQ][1][1] === true) {
+            secSuggestion.style.backgroundImage = 'url(images/true.png)';
+    }else if(suggestions[randomQ][2][1] === true) {
+            thrSuggestion.style.backgroundImage = 'url(images/true.png)';
+    }
+    },13000)
+
+    setTimeout(()=>{
         frsSuggestion.style.backgroundImage = 'url(images/suggestion.png)';
+        secSuggestion.style.backgroundImage = 'url(images/suggestion.png)';
+        thrSuggestion.style.backgroundImage = 'url(images/suggestion.png)';
         randomQ++;
         pickSuestion(randomQ);
     },17000)
-    
 
     if(randomQ === quiz.length) {
         pickSuestion(0);
@@ -104,7 +166,18 @@ secSuggestion.addEventListener("click",()=> {
     },11000)
 
     setTimeout(()=>{
+    if(suggestions[randomQ][0][1] === true) {
+        frsSuggestion.style.backgroundImage = 'url(images/true.png)';
+    }else if(suggestions[randomQ][2][1] === true) {
+        thrSuggestion.style.backgroundImage = 'url(images/true.png)';
+    }
+
+    },13000)
+
+    setTimeout(()=>{
+        frsSuggestion.style.backgroundImage = 'url(images/suggestion.png)';
         secSuggestion.style.backgroundImage = 'url(images/suggestion.png)';
+        thrSuggestion.style.backgroundImage = 'url(images/suggestion.png)';
         randomQ++;
         pickSuestion(randomQ);
     },17000)
@@ -128,7 +201,18 @@ thrSuggestion.addEventListener("click",()=> {
     }
     },11000)
 
+    setTimeout(()=>{   
+    if(suggestions[randomQ][0][1] === true) {
+        frsSuggestion.style.backgroundImage = 'url(images/true.png)';
+    }else if(suggestions[randomQ][1][1] === true) {
+        secSuggestion.style.backgroundImage = 'url(images/true.png)';
+    }
+
+    },13000)
+
     setTimeout(()=>{
+        frsSuggestion.style.backgroundImage = 'url(images/suggestion.png)';
+        secSuggestion.style.backgroundImage = 'url(images/suggestion.png)';
         thrSuggestion.style.backgroundImage = 'url(images/suggestion.png)';
         randomQ++;
         pickSuestion(randomQ);
